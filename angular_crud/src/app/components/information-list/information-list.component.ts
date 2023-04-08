f02c51ad1a6d896881a200252f413e1337ea3d6a
saiful Islam <saiful.islam5@northsouth.edu>
2023-04-08 12:38:17 +0600

    material modal design

--- angular_crud/src/app/components/information-list/information-list.component.ts
+++ angular_crud/src/app/components/information-list/information-list.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { informationModel } from 'src/app/model/informationmodel';
+import { MatDialog } from '@angular/material/dialog';
+import { PopupComponent } from '../popup/popup.component';
 @Component({
   selector: 'app-information-list',
   templateUrl: './information-list.component.html',
   styleUrls: ['./information-list.component.scss']
 })
 export class InformationListComponent implements OnInit {
-  constructor(){}
+  constructor(private dialog: MatDialog){}
+
   tabledata!:informationModel[];
   ngOnInit(): void {
 
   }
   displayColums: string[] = ["_id", "name", "phone", "email", "action"]
+  Openpopup(id:any){
+    this.dialog.open(PopupComponent, {
+      width: '500px',
+      exitAnimationDuration: '1000ms',
+      enterAnimationDuration: '1000ms',
+      data: {
+        id: id
+      }
+    })
+  }
 
 }
